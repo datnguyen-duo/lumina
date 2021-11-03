1348ae00ca4ffdb3bb314ec608ea35784a418c3a
Dat Nguyen <dat@duo-studio.co>
2021-11-02 22:14:43 -0400

    donate styles + initial build

--- templates/tmplt-donate.php
+++ templates/tmplt-donate.php
@@ -9,42 +9,46 @@ get_header(); ?>
             </div>
             <div class="container">
                 <div class="row">
-                    <h4>Donation Preferences</h4>
+                    <h2>Donation Preferences</h2>
                     <div class="form-wrapper">
-                        <p>I want to contribute:</p>
-                        <?php echo do_shortcode('[wc_woo_donation id="55"]')?>
-                        <p>to Lumina Studio Theatre</p>
-                        <div class="btn-wrapper">
-                            <div class="btn">This is a one time donation</div>
-                            <div class="btn">I would like to make this a recurring donation</div>
+                        <div class="inner">
+                            <h4>I want to contribute:</h4>
+                            <?php echo do_shortcode('[wc_woo_donation id="55"]')?>
+                            <h4>to Lumina Studio Theatre</h4>
                         </div>
-                        <div class="btn-wrapper">
-                            <div class="btn">Monthly</div>
-                            <div class="btn">Quarterly</div>
-                            <div class="btn">Annually</div>
+                        <div class="inner">
+                            <div class="btn-wrapper">
+                                <div class="btn pill">This is a one time donation</div>
+                                <div class="btn pill">I would like to make this a recurring donation</div>
+                            </div>
+                            <div class="btn-wrapper">
+                                <div class="btn pill">Monthly</div>
+                                <div class="btn pill">Quarterly</div>
+                                <div class="btn pill">Annually</div>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="row">
-                    <h4>Privacy Preferences</h4>
+                    <h2>Privacy Preferences</h2>
                     <div class="btn-wrapper">
-                        <div class="btn">My full contact information</div>
-                        <div class="btn">My name and email only</div>
-                        <div class="btn">Anonymous</div>
+                        <div class="btn pill">My full contact information</div>
+                        <div class="btn pill">My name and email only</div>
+                        <div class="btn pill">Anonymous</div>
                     </div>
                 </div>
                 <div class="row">
-                    <h4>Designation (Optional)</h4>
+                    <h2>Designation (Optional)</h2>
                     <p>To designate your donation for a specific fund or purpose, please enter a description of how you'd like your donation to be used.</p>
-                    <input type="text">
+                    <input type="text" class="full-width">
                 </div>
                 <div class="row">
-                    <h4>Designation (Optional)</h4>
-                    <p>To designate your donation for a specific fund or purpose, please enter a description of how you'd like your donation to be used.</p>
-                    <input type="text">
+                    <h2>Dedication Or Gift(Optional)</h2>
+                    <p>To make a donation on behalf of or in memory of another person, please enter the person’s name. You will have a chance to send an eCard to this person at the end of making a donation.</p>
+                    <input type="text" class="full-width">
                 </div>
                 <div class="row">
-                    <button>Add to Cart</button>
+                    <button class="pill">Add to Cart</button>
                 </div>
             </div>
         </div>
