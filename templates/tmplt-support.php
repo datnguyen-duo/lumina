4797a8172602c10ab539ceaa82b2534983291221
Dat Nguyen <dat@duo-studio.co>
2021-11-26 19:19:51 -0500

    support page content

--- templates/tmplt-support.php
+++ templates/tmplt-support.php
@@ -20,21 +20,18 @@ get_header();
             <div class="left">
                 <div class="description">
                     <p>
-                        For nearly 25 years, Lumina has been helping young actors discover the wonders of the theatre’s spotlight!
-                        For 200 productions, the youth of our community have been introduced to Shakespeare and helped to develop
-                        an appreciation of the classics and a love of language.
+                    For nearly 25 years, Lumina has been helping young actors discover the wonders of the theatre’s spotlight! For 200 productions, the youth of our community have been introduced to Shakespeare and helped to develop an appreciation of the classics and a love of language.
                     </p>
 
                     <p>
-                        More than ever, your donation is needed to help Lumina continue to bring the transformative power of
-                        theatre to audiences and actors of all ages throughout our community.
+                        More than ever, your donation is needed to help Lumina continue to bring the transformative power of theatre to audiences and actors of all ages throughout our community.
                     </p>
                 </div>
             </div>
             <div class="right">
                 <div class="image_holder">
                     <div class="image">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/dev/hero_image.jpg" alt="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/dev/Nicholas Nickleby.jpg" alt="">
                     </div>
                 </div>
             </div>
@@ -52,7 +49,7 @@ get_header();
             <ul>
                 <li>Click on the Donate Now link below to make a donation through our website</li>
                 <li>Donations may also be sent directly to: Lumina Studio Theatre, 8641 Colesville Road, Silver Spring, MD 20910</li>
-                <li>Lumina Studio Theatre is a tax-exempt 501(c)(3) organization. Gifts are tax deductible to the extent allowed by law. Please consult your personal financial advisor.</li>
+                <li>Lumina Studio Theatre is a tax-exempt 501(c)(3) organization. Gifts are tax deductible to the extent allowed by law.</li>
             </ul>
         </div>
     </section>
@@ -80,10 +77,7 @@ get_header();
                         <div class="description_holder">
                             <h3>Cash Gifts</h3>
                             <p>
-                                Lumina depends on the generosity of individuals like you as well as
-                                public and private foundations to help promote its mission and grow
-                                the organization.  Contributions to the Annual Fund help with the
-                                costs of productions and operating costs.
+                                Lumina depends on the generosity of individuals like you as well as public and private foundations to help promote its mission and grow the organization. Contributions to the Annual Fund help with the costs of productions and operating costs. <a href="/donate/">Donate Now.</a>
                             </p>
                         </div>
                     </div>
@@ -96,9 +90,7 @@ get_header();
                         <div class="description_holder">
                             <h3>Scholarship Gifts</h3>
                             <p>
-                                Lumina believes that no young person should miss the opportunity to participate in a production due
-                                to financial constraints. A portion of funds raised from the Annual Campaign go directly to assist
-                                qualifying young actors to participate in Lumina productions.
+                            Lumina believes that no young person should miss the opportunity to participate in a production due to financial constraints. A portion of funds raised from the Annual Campaign go directly to assist qualifying young actors to participate in Lumina productions. Donations towards the general scholarship fund or to support a scholarship actor for an entire season are gratefully accepted. If you would like your donation to be used specifically for scholarships, make a note in the text box on the donation link. <a href="/donate/">Donate Now.</a>
                             </p>
                         </div>
                     </div>
@@ -111,8 +103,7 @@ get_header();
                         <div class="description_holder">
                             <h3>Memory Gifts</h3>
                             <p>
-                                The Jillian Raye Memorial Fund was established to provide funds to specifically further
-                                the development of Lumina’s mission.
+                            The Jillian Raye Memorial Fund was established to provide funds to specifically further the development of Lumina’s mission. If you would like your donation to go into the Jillian Raye Memorial Fund, make a note in the text box on the donation link. <a href="/donate/">Donate Now.</a>
                             </p>
                         </div>
                     </div>
@@ -125,8 +116,7 @@ get_header();
                         <div class="description_holder">
                             <h3>Volunteering</h3>
                             <p>
-                                Lumina is authorized to offer Student Service Learning Hours (SSL) towards
-                                Montgomery County Schools graduation requirements.
+                                Lumina is authorized to offer Student Service Learning Hours (SSL) towards Montgomery County Schools graduation requirements.
                             </p>
                         </div>
                     </div>
@@ -152,11 +142,9 @@ get_header();
                         </div>
 
                         <div class="description_holder">
-                            <h3>Planed Giving</h3>
+                            <h3>Planned Gifts</h3>
                             <p>
-                                Create your own legacy by making Lumina a part of your trust, bequest, or endowment.
-                                This type of gift guarantees Lumina will continue to fulfill its mission.
-                                For more information, contact the office by calling 301-565-2281 or send an email.
+                            Create your own legacy by making Lumina a part of your trust, bequest, or endowment. This type of gift guarantees Lumina will continue to fulfill its mission. Contact the office by calling 301-565-2281 or sending an email to <a href="mailto:office@luminastudio.org">office@luminastudio.org</a>.
                             </p>
                         </div>
                     </div>
@@ -168,9 +156,7 @@ get_header();
                         <div class="description_holder">
                             <h3>Stock Gifts</h3>
                             <p>
-                                Lumina accepts gifts of publicly-traded stocks and bonds as per IRS guidelines. Gifts of
-                                stocks or bonds may be sold and the proceeds used to support productions and programming.
-                                For more information, contact the office by calling 301-565-2281 or sending an email.
+                            Lumina accepts gifts of publicly-traded stocks and bonds as per IRS guidelines. Gifts of stocks or bonds may be sold and the proceeds used to support productions and programming. For more information on how to donate stocks or bonds, contact the office by calling 301-565-2281 or sending an email to <a href="mailto:office@luminastudio.org">office@luminastudio.org</a>.
                             </p>
                         </div>
                     </div>
@@ -191,11 +177,9 @@ get_header();
                         </div>
 
                         <div class="description_holder">
-                            <h3>Planed Giving</h3>
+                            <h3>Employee Matching Gifts</h3>
                             <p>
-                                Create your own legacy by making Lumina a part of your trust, bequest, or endowment.
-                                This type of gift guarantees Lumina will continue to fulfill its mission.
-                                For more information, contact the office by calling 301-565-2281 or send an email.
+                            Many employers match donations to charitable organizations from their employees. These matching gifts can double your donation to Lumina Studio Theatre. Contact the Human Resources Department at your business to find out if there is a matching gift program. Send the Matching Gift form to: Lumina Studio Theatre, 8641 Colesville Rd, Silver Spring, MD 20910.
                             </p>
                         </div>
                     </div>
