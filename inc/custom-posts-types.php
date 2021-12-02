a133b3fa5c7393bdc6c5fe19d0692188571544b7
Nemanja <nemanja@systemstudio.co>
2021-12-02 14:13:54 +0100

    members custom post type added

--- inc/custom-posts-types.php
+++ inc/custom-posts-types.php
@@ -41,5 +41,26 @@ function register_post_types() {
         'supports' => array('title','thumbnail'),
         'menu_icon' => 'dashicons-calendar-alt',
     ));
+
+    register_post_type('members',array(
+        'labels' => array(
+            'name' => _x('Members', 'post type general name'),
+            'singular_name' => _x('Member', 'post type singular name'),
+            'menu_name' => 'Members'
+        ),
+        'rewrite' => array(
+            'slug' => 'members',
+            'with_front' => false
+        ),
+        'public' => true,
+        'publicly_queryable' => false,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'query_var' => true,
+        'has_archive' => false,
+//        'hierarchical' => true,
+        'supports' => array('title','thumbnail'),
+        'menu_icon' => 'dashicons-admin-users',
+    ));
 }
 add_action('init','register_post_types');
